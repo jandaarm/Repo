394e0cd4cec9bdc6be36c4ec59c6e9d72844ccba
jandaarm <jantasovdaulet07@gmail.com>
2026-02-11 18:03:53 +0500

    Assignment 4 project

---
+++ src/main/java/com/example/as4/As4Application.java
@@ -0,0 +1,18 @@
+package com.example.as4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class As4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(As4Application.class, args);
+	}
+
+}
+
+/* help:
+@Component - tells that objects of this class will be created by Spring(not User)
+
+ */
