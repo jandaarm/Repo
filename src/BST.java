80104663184653a5e11c80e200a2198f27f1eab2
jandaarm <jantasovdaulet07@gmail.com>
2026-05-02 20:06:04 +0500

    HashTable WIP, BST done

--- src/BST.java
+++ src/BST.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class BST<K extends Comparable<K>,V>{
+public class BST<K extends Comparable<K>,V> implements Iterable<BST.Entry<K,V>>{
     private BSTNode<K,V> root;
     private int size = 0;
     private static class BSTNode<K,V> {
@@ -13,6 +14,9 @@ public class BST<K extends Comparable<K>,V>{
             this.value = value;
         }
     }
+
+    public int size() {return size;}
+
     public void put(K key, V value){
         BSTNode<K,V> newNode = new BSTNode<>(key, value);
         BSTNode<K,V> current = root;
@@ -57,21 +61,97 @@ public class BST<K extends Comparable<K>,V>{
         }
         return null;
     }
-
-    public Iterable<K> iterator(){
+    public void delete(K key){
+        BSTNode<K,V> current = root;
+        BSTNode<K,V> parent = null;
+        while(current != null){
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0){
+                parent = current;
+                current = current.left;
+            } else if (cmp > 0){
+                parent = current;
+                current = current.right;
+            } else {
+                if (current.right != null && current.left != null){
+                    BSTNode<K,V> p = current;
+                    BSTNode<K,V> successor = current.right;
+                    while(successor.left != null){
+                        p = successor;
+                        successor = successor.left;
+                    }
+                    current.key = successor.key;
+                    current.value = successor.value;
+                    if (p.left == successor) {
+                        p.left = successor.right;
+                    } else {
+                        p.right = successor.right;
+                    }
+                    size--;
+                    break;
+                } else{
+                    BSTNode<K,V> child = (current.left != null) ? current.left : current.right;
+                    if (parent == null){
+                        root = child;
+                    } else if(parent.left == current){
+                        parent.left = child;
+                    } else {
+                        parent.right = child;
+                    }
+                    size--;
+                    break;
+                }
+            }
+        }
     }
 
-    private class MyIterator implements Iterator<K> {
+    public Iterator<Entry<K,V>> iterator(){return new MyIterator();}
+
+    private class MyIterator implements Iterator<Entry<K,V>> {
+        MyStack<BSTNode<K,V>> trace = new MyStack<>();
+
+        MyIterator(){
+            BSTNode<K,V> current = root;
+            while(current != null){
+                trace.push(current);
+                current = current.left;
+            }
+        }
 
         @Override
         public boolean hasNext() {
-            return false;
+            return !trace.empty();
         }
 
         @Override
-        public K next() {
-            return null;
+        public Entry<K,V> next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            BSTNode<K,V> nextItem = trace.pop();
+            BSTNode<K,V> current = nextItem;
+            if (current.right != null){
+                current = current.right;
+                while(current != null){
+                    trace.push(current);
+                    current = current.left;
+                }
+            }
+            return new Entry<>(nextItem.key, nextItem.value);
         }
+    }
+
+    public static class Entry<K,V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() { return key; }
+        public V getValue() { return value; }
     }
 
     public void inOrder(){
--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,14 @@ public class Main{
         tree.put(15, "Kokshetau");
         tree.put(24, "Astana");
 
+        System.out.println("Size: " + tree.size());
+        System.out.println(tree.get(13));
+        System.out.println("Delete 13");
+        tree.delete(13);
+        for (var element : tree) {
+
+            System.out.println("key is " + element.getKey() + " and value is " + element.getValue());
+
+        }
     }
 }
